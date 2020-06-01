8d3fe2e1b4a4a8c5e87b99df04f9e809e4ddc24a
Tomaz Canabrava <tcanabrava@kde.org>
2020-06-01 18:32:02 +0200

    Add Typing

--- __init__.py
+++ __init__.py
@@ -2,21 +2,21 @@ from mycroft import MycroftSkill, intent_file_handler
 
 
 class RedditSkill(MycroftSkill):
-    def __init__(self):
+    def __init__(self) -> None:
         MycroftSkill.__init__(self)
 
-    def initialize(self):
+    def initialize(self) -> None:
         self.register_entity_file('data.entity')
         self.register_intent_file('reddit_download.intent', self.handle_reddit_download)
         self.register_intent_file('reddit_show.intent', self.handle_reddit_show)
 
 
-    def handle_reddit_show(self, message):
+    def handle_reddit_show(self, message) -> None:
         self.speak_dialog("reddit_show")
 
-    def handle_reddit_download(self, message):
+    def handle_reddit_download(self, message) -> None:
         self.speak_dialog("reddit_download")
 
-def create_skill():
+def create_skill() -> RedditSkill:
     return RedditSkill()
 
