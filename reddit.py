9f3ca41f1f0383ae4c73a6a33994e1d4a2abcfb8
Tomaz Canabrava <tcanabrava@kde.org>
2020-06-09 20:03:52 +0200

    Update the reddit library to handle downloads of photos and videos

--- reddit.py
+++ reddit.py
@@ -42,7 +42,7 @@ class Reddit:
             self.mycroft.log.info(f"Getting replies")
         except Exception as e:
             self.mycroft.log.info(f"Got zero replies {e}")
-            result = [] 
+            result = []
         finally:
             return result
         #
@@ -50,83 +50,163 @@ class Reddit:
 
 
     def get_file_name(self, item):
-        filetype = item.url.split('.')[-1]    
+        filetype = item.url.split('.')[-1]
         filename = item.title + '_' + item.id + '.' + filetype
         for char in ['(', ')', ',', '@', '!', '?', '/', ';', '"', "'"]:
             filename = filename.replace(char, '')
+        #
+
         filename = filename.replace(' ', '_')
 
         asciidata = filename.encode("ascii","ignore").decode("utf-8")
         asciidata = '_'.join(asciidata.split('_'))
         return asciidata
+    #
 
 
-    def save_image(self, item:str, folder: str):
-        filetype = item.url.split('.')[-1]    
+    def save_image(self, item:str, folder: str) -> bool:
+        filetype = item.url.split('.')[-1]
 
         if not filetype.lower() in ['jpg', 'png', 'gif', 'jpeg']:
-            self.mycroft.log.info(f"File does not have a filetype:{item.url}")
-            return
+            return False
+        #
 
         filename = self.get_file_name(item)
         full_file_path = os.path.join(folder, filename)
         if os.path.exists(full_file_path):
-            self.mycroft.log.info("[File Exists]", filename)
-            return
+            self.mycroft.log.info(f"[File Exists] {filename}")
+            return False
+        #
 
         try:
             response = requests.get(item.url)
             if not response.ok:
                 self.mycroft.log.info("Error downloading file.")
-                return
+                return False
         except Exception as e:
             self.mycroft.log.info(f"Error downloading path {e}")
-            return
+            return False
+        #
 
         self.mycroft.log.info(f"[File Saved] {filename}")
         pathlib.Path(full_file_path).write_bytes(response.content)
 
+        return True
+    #
 
-    def save_video(self, item:str, folder:str) -> None:
+    def save_video(self, item:str, folder:str) -> bool:
         self.mycroft.log.info(f"[Video] Trying to download {item.url} to {folder}")
-        if 'gfycat' in item.url or 'youtube' in item.url:
-            subprocess.call(['youtube-dl', item.url], cwd=folder)
+        should_download = False
+        for accepted_content in ["youtube", "gfycat"]:
+            if accepted_content in item.url:
+                should_download = True
+
+        if not should_download:
+            return False
+        #
+
+        return subprocess.call(['youtube-dl', item.url], cwd=folder) == 0
+    #
+
+    def download_photos_and_videos(self,
+        data_type,
+        community: str,
+        video_folder:str,
+        photos_folder:str,
+        max_images:int,
+        max_videos:int) -> None:
+
+        current_images = 0
+        current_videos = 0
+
+        try:
+            community_posts = self.get_reddit_replies(community)
+        except Exception as e:
+            self.mycroft.speak(f"Unable to reach reddit, verify the reddit skill configuration")
+            return
+        #
+
+        for item in community_posts:
+            try:
+                if (data_type in [Reddit.DataTypes.ALL, Reddit.DataTypes.VIDEO]
+                    and current_videos < max_videos
+                    and self.save_video(item=item, folder=video_folder)
+                ):
+                    current_videos += 1
+                #
+
+                if (data_type in [Reddit.DataTypes.ALL, Reddit.DataTypes.IMAGES]
+                    and current_images < max_images
+                    and self.save_image(item=item, folder=photos_folder)
+                ):
+                    current_images += 1
+                #
+            except Exception as e:
+                    self.mycroft.log.info(f"Error trying to get {community}:")
+                    self.mycroft.log.info(f"{e}")
+            #
+
+            # Break early from the loop if we finished downloading things
+            if data_type == Reddit.DataTypes.IMAGES and current_images >= max_images:
+                return
+            #
+
+            if data_type == Reddit.DataTypes.VIDEO and current_videos >= max_videos:
+                return
+            #
+
+            if (data_type == Reddit.DataTypes.ALL
+                and current_videos >= max_videos
+                and current_images >= max_images
+            ):
+                return
+            #
+        #
+    #
 
 
-    def download_all(self, data_type, communities: List[str]) -> None:
-        self.mycroft.speak(f"Inside of the reddit library {data_type}")
+    def download(self,
+        data_type,
+        communities: List[str],
+        download_folder:str,
+        max_images:int,
+        max_videos:int) -> None:
+
+        self.mycroft.log.info(f"Inside of the reddit library {data_type}")
         total = len(communities)
-        for arg in communities:
-            idx = communities.index(arg)
-            self.mycroft.speak(f"Trying to get {arg}")
+        for community in communities:
+            idx = communities.index(community)
+            self.mycroft.log.info(f"Trying to get {community}")
 
-            if data_type in [Reddit.DataTypes.ALL, Reddit.DataTypes.VIDEO]:
-                folder_video = os.path.join(self.download_folder, "Reddit", "videos", arg)
-                pathlib.Path(folder_video).mkdir(parents=True, exist_ok=True)
+            video_folder = folder(
+                download_folder=download_folder,
+                subfolder="video",
+                community=community)
 
-            if data_type in [Reddit.DataTypes.ALL, Reddit.DataTypes.IMAGES]:
-                folder_pic = os.path.join(self.download_folder, "Reddit", "pictures", arg)
-                pathlib.Path(folder_pic).mkdir(parents=True, exist_ok=True)
-                self.mycroft.speak(f"[{idx} of {total}]")
+            photos_folder = folder(
+                download_folder=download_folder,
+                subfolder="images",
+                community=community)
 
-            for item in self.get_reddit_replies(arg):
-                try:
-                    if data_type in [Reddit.DataTypes.ALL, Reddit.DataTypes.VIDEO]:
-                        self.save_video(item=item, folder=folder_video)
-
-                    if data_type in [Reddit.DataTypes.ALL, Reddit.DataTypes.IMAGES]:
-                        self.save_image(item=item, folder=folder_pic)
-
-                except Exception as e:
-                    print(f"Error trying to get {arg}: {e}")
+            self.download_photos_and_videos(
+                data_type=data_type,
+                community=community,
+                video_folder=video_folder,
+                photos_folder=photos_folder,
+                max_images=max_images,
+                max_videos=max_videos
+            )
+        #
+    #
+#
 
 
 def main():
     parser = argparse.ArgumentParser(description='Download Pictures and videos from a Reddit Repository.')
     parser.add_argument(
-        '--reddits', 
-        metavar='N', 
-        type=str, 
+        '--reddits',
+        metavar='N',
+        type=str,
         nargs='+',
         help='list of reddits to download')
 
@@ -136,7 +216,9 @@ def main():
     reddit = Reddit()
     reddit.download_all('/home/tcanabrava/coiso', communities)
     return 0
+#
 
 if __name__ == "__main__":
     exit(main())
+#
 
